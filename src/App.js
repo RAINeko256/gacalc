e02b0fa0cd85b762dbedae06382a6b0252ddc94c
RAINeko256 <reirain256@gmail.com>
2021-02-24 01:28:21 +0900

    初期化

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import {makeStyles} from '@material-ui/core/styles';
+
+//ヘッダー
+function Header(){
+  return(
+    <div className="header">
+      <div>
+      <p className="h_title">GAcalc</p>
+      <p className="h_sub_title">Genshin Ascensions calculator</p>
+      </div>
+    </div>
+  )
+}
+
+//コンテンツ
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+function Calc(){
+  const classes = useStyles();
+
+  return(
+  <div className="main">
+    <form className={classes.root} noValidate autoComplete="off">
+    <TextField
+    id="outlined-number"
+    label="Number"
+    type="number"
+    InputLabelProps={{
+        shrink: true,
+    }}
+    variant="outlined"
+    />
+    </form>
+  </div>
+  );
+}
+//フッター
+function Footer(){
+  return(
+    <div className="footer">
+      <p>お問い合わせはDMまで</p>
+      <div className="item">
+        <a href="https://twitter.com/RAINEKO256">
+          <div>
+          <i class="fab fa-twitter fa-2x twitter"></i>
+          <p>@RAINeko256</p>
+          </div>
+        </a>
+      </div>
+      <p>Copyrights RAINeko</p>
+      
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Calc/>
+      <Footer/>
     </div>
   );
 }
