090ab14c931a7a581517c67885b72d098116bb7d
RAINEKO256 <reirain256@gmail.com>
2021-03-18 00:26:11 +0900

    fix:footerの改善
    GitHubのリンクの追加
    copyrightsの(c)を追加
    ダサい下線の削除

--- src/js/components/block/footer.js
+++ src/js/components/block/footer.js
@@ -6,17 +6,27 @@ function Footer() {
     return (
       <div className="footer">
         <div>
-        <p>お問い合わせはDMまで</p>
-        <div className="f_item">
-          <a href="https://twitter.com/RAINEKO256">
-            <Button variant="contained">
-              <i class="fab fa-twitter fa-2x twitter"></i>
-              <p>@RAINeko256</p>
-            </Button>
-          </a>
-        </div>
-        <p>※当サイトの内容により生じた損害等の一切について責任を負いかねますのでご了承ください。</p>
-        <p>Copyrights RAINeko</p>
+          <p>お問い合わせはDMまで</p>
+          <div className="f_item_box">
+            <div className="f_item">
+              <a href="https://twitter.com/RAINEKO256">
+                <Button variant="contained">
+                  <i class="fab fa-twitter fa-2x"></i>
+                  <p>@RAINeko256</p>
+                </Button>
+              </a>
+            </div>
+            <div className="f_item">
+              <a href="https://github.com/RAINeko256/gacalc">
+                <Button variant="contained">
+                  <i class="fab fa-github fa-2x"></i>
+                  <p>@RAINeko256</p>
+                </Button>
+              </a>
+            </div>
+          </div>
+          <p>※当サイトの内容により生じた損害等の一切について責任を負いかねますのでご了承ください。</p>
+          <p>Copyrights (c) RAINeko</p>
         </div>
       </div>
     );
