9bc4c6fdd3a291437c3c367f1dd0e8c37fbb3335
RAINEKO256 <reirain256@gmail.com>
2021-03-30 00:43:57 +0900

    design:fontawesomeの導入とリンクボタンの変更
    fontawesomeを導入した
    TwitterとGitHubのリンクのボタンを変更した

--- src/js/components/block/footer.js
+++ src/js/components/block/footer.js
@@ -1,35 +1,51 @@
 import "style/block/Footer.css";
 
 import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter , faGithub} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
-    return (
-      <div className="footer">
-        <div>
-          <p>お問い合わせはDMまで</p>
-          <div className="f_item_box">
-            <div className="f_item">
-              <a href="https://twitter.com/RAINEKO256">
-                <Button variant="contained">
-                  <i class="fab fa-twitter fa-2x"></i>
-                  <p>@RAINeko256</p>
-                </Button>
-              </a>
-            </div>
-            <div className="f_item">
-              <a href="https://github.com/RAINeko256/gacalc">
-                <Button variant="contained">
-                  <i class="fab fa-github fa-2x"></i>
-                  <p>@RAINeko256</p>
-                </Button>
-              </a>
-            </div>
+  return (
+    <div className="footer">
+      <div>
+        <p>お問い合わせはDMまで</p>
+        <div className="f_item_box">
+          <div className="f_item">
+            <a href="https://twitter.com/RAINEKO256">
+              <Fab
+                variant="extended"
+                size="medium"
+                color="secondary"
+                aria-label="Twitter"
+              >
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+                RAINEKO256
+              </Fab>
+            </a>
           </div>
-          <p>※当サイトの内容により生じた損害等の一切について責任を負いかねますのでご了承ください。</p>
-          <p>Copyrights (c) RAINeko</p>
+          <div className="f_item">
+            <a href="https://github.com/RAINeko256/gacalc">
+              <Fab
+                variant="extended"
+                size="medium"
+                color="secondary"
+                aria-label="GitHub"
+              >
+                <FontAwesomeIcon icon={faGithub} size="2x"/>
+                RAINeko256
+              </Fab>
+            </a>
+          </div>
         </div>
+        <p>
+          ※当サイトの内容により生じた損害等の一切について責任を負いかねますのでご了承ください。
+        </p>
+        <p>Copyrights (c) RAINeko</p>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Footer;
